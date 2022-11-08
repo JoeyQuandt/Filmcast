abf3d34367d16c73275a6ed01aca0deb8ab1683a
Joey Quandt <57623466+JoeyQuandt@users.noreply.github.com>
2022-11-08 15:55:34 +0100

    code improvement/cleaning

---
+++ utils.js
@@ -0,0 +1,40 @@
+const header = document.querySelector('.header-slider')
+const navBar = document.querySelector("nav")
+const genreSliders = document.querySelectorAll(".genre-slider")
+
+/*Blaze slider*/
+function ActivateSlider(){
+    new BlazeSlider(header)
+
+    genreSliders.forEach(el => {
+    new BlazeSlider(el,{
+        all: {
+        enableAutoplay: false,
+        slidesToScroll: 3,
+        slidesToShow: 5,
+        transitionDuration: 300
+        },
+        "(max-width: 900px)": {
+        slidesToShow: 2,
+        slidesToShow: 2,
+        slidesGap: "40px"
+        },
+        "(max-width: 500px)": {
+        slidesToShow: 2,
+        slidesToScroll: 2
+        }
+    })
+    })
+}
+
+
+/*Scrolling active*/
+window.addEventListener("scroll",function(){
+    let windowPosition = window.scrollY>0;
+
+    navBar.classList.toggle("navigation-section-scroll-active",windowPosition)
+})
+
+export {ActivateSlider}
+
+
